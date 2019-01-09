76ccbe4cb69375e6029c0d02d9120c74fb9694b7
Dreamwalker <aristojeff@gmail.com>
2019-01-09 13:00:29 +0900

    Create 7_Reverse Integer.java

---
+++ 7_Reverse Integer.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int reverse(int x) {
+        boolean sign = x > 0;
+        x = Math.abs(x);
+        int res =0;
+        while(x > 0){
+            if(res > Integer.MAX_VALUE / 10) return 0;
+            res = res * 10 + x % 10;
+            x = x / 10;
+        }
+        return sign ? res : -res;
+    }
+}
+
+class Solution {
+    public int reverse(int x) {
+        int rev = 0;
+        while (x != 0) {
+            int pop = x % 10;
+            x /= 10;
+            if (rev > Integer.MAX_VALUE/10 || (rev == Integer.MAX_VALUE / 10 && pop > 7)) return 0;
+            if (rev < Integer.MIN_VALUE/10 || (rev == Integer.MIN_VALUE / 10 && pop < -8)) return 0;
+            rev = rev * 10 + pop;
+        }
+        return rev;
+    }
+}
